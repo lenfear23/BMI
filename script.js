9bd5c62a25c206e49a200cfec3147557cc37b3e2
Lenfear23 <pappd377@gmail.com>
2022-09-20 14:42:35 +0200

    Új design és üres mező esetén alert jelzi a hibát

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function szamitas(){
- 
- let mag = parseFloat(document.getElementById("magassag").value);
- let suly = parseFloat(document.getElementById("suly").value);
+    let mag = parseFloat(document.getElementById("magassag").value);
+    let suly = parseFloat(document.getElementById("suly").value);
+    if(document.getElementById("magassag").value == "" || document.getElementById("suly").value == ""){
+        document.getElementById("baj").style.visibility = "visible";
+        
+    }
+    else{
+        document.getElementById("baj").style.visibility = "hidden";
+      
+        let ered = suly / (mag / 100) ** 2;
 
- let ered = suly / (mag / 100) ** 2;
-
- document.getElementById("eredmeny").textContent = ered;
-
+        document.getElementById("eredmeny").textContent = ered;
+    }
 }
 
-document.getElementById("laci").addEventListener('click',szamitas);
+document.getElementById("btn").addEventListener('click',szamitas);
